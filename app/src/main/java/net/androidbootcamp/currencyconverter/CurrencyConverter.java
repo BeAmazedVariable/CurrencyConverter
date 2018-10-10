19ae52b82c199ae862509acd4aedd29aaaa12841
Amazing <thienphuoc.0108@gmail.com>
2018-10-10 18:39:47 -0400

    Wait for the logo

--- app/src/main/java/net/androidbootcamp/currencyconverter/CurrencyConverter.java
+++ app/src/main/java/net/androidbootcamp/currencyconverter/CurrencyConverter.java
@@ -7,51 +7,71 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
 public class CurrencyConverter extends AppCompatActivity {
-    double cadToUsd = 0.772642;
-    double cadToEur = 0.672080;
-    double cadToInr = 57.4085;
-    double cadToVnd = 18064.17;
-    double cadAmount;
-    double convertResult;
-    int countryChoice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_converter);
 
+        //Initiate instance of an object of the layout file
         final EditText cad= findViewById(R.id.txtCad);
         final Spinner country= findViewById(R.id.spnCountries);
         Button convert= findViewById(R.id.btnConvert);
         final TextView result= findViewById(R.id.txtResult);
 
+        //Make actions when the user clicks the button
         convert.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                cadAmount= Double.parseDouble(cad.getText().toString());
+            public void onClick(View v)
+            {
+                //Declare constant variable for the convert rate with appropriate countries
+                final double cadToUsd = 0.772642;
+                final double cadToEur = 0.672080;
+                final double cadToInr = 57.4085;
+                final double cadToVnd = 18064.17;
 
+                //Declare variable to collect user input in CAD and the country has been selected to calculate the selected country currency
+                double cadAmount;
+                double convertResult = 0;
+                int countryChoice;
+
+                //Collect the amount of money that the user has entered and the country that has been selected
+                cadAmount= Double.parseDouble(cad.getText().toString());
                 countryChoice=country.getSelectedItemPosition();
 
-                switch(countryChoice)
+                //Decision making statement to make sure the user input lower than $100000
+                if (cadAmount <100000)
                 {
-                    case 0:
-                        convertResult = cadAmount * cadToUsd;
-                        break;
-                    case 1:
-                        convertResult = cadAmount * cadToEur;
-                        break;
-                    case 2:
-                        convertResult = cadAmount * cadToInr;
-                        break;
-                    case 3:
-                        convertResult = cadAmount * cadToVnd;
-                        break;
+                    //Decision making statements decide which convert rate should be use to calculate the appropriate amount money in the selected country
+                    switch(countryChoice)
+                    {
+                        case 0:
+                            convertResult = cadAmount * cadToUsd;
+                            break;
+                        case 1:
+                            convertResult = cadAmount * cadToEur;
+                            break;
+                        case 2:
+                            convertResult = cadAmount * cadToInr;
+                            break;
+                        case 3:
+                            convertResult = cadAmount * cadToVnd;
+                            break;
+                    }
+                }
+                else
+                {
+                    //Make a toast notification when the user input more than $100,000
+                    Toast.makeText(CurrencyConverter.this,"We can't convert more than $100,000CAD",Toast.LENGTH_LONG).show();
                 }
                 DecimalFormat currency =new DecimalFormat("$###,###.##");
 
+                //Indicate the appropriate currency in the selected country
                 result.setText(currency.format(cadAmount) + " equals to " + currency.format(convertResult));
             }
         });
